ad3ef80f63964e1f4e80a32bcddb966dccfc4f37
Gautam Sharma <gautam2586272@gmail.com>
2024-12-11 17:49:36 +0530

    final changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,96 @@ import {
   Inject,
   Day,
   Week,
-  WorkWeek,
   Month,
   Agenda,
   ScheduleComponent,
   EventSettingsModel,
+  DragAndDrop,
+  Resize,
+  DragEventArgs,
+  ResizeEventArgs,
+  NavigateOptions,
+  ViewDirective,
+  ViewsDirective
 } from "@syncfusion/ej2-react-schedule";
 
-const App = () => {
+const onDragStart = (args: DragEventArgs) => {
+  args.interval = 1;
+  (args.navigation as NavigateOptions).enable = true;
+};
+
+const onResizeStart = (args: ResizeEventArgs) => {
+  args.interval = 1;
+};
+
+const eventTemplate = (props: { Subject?: string; StartTime?: Date; EndTime?: Date; Color?: string }): JSX.Element => {
+  return <div className="template-wrap">{props.Subject}</div>;
+};
+
+const eventTemplateMonth = (props: { Subject?: string }): JSX.Element => {
+  return <div className="template-wrap1">{props.Subject}</div>;
+};
+
+const App = () => {4
   const eventData =  [{
       Id: 1,
       Subject: 'Explosion of Betelgeuse Star',
       StartTime: new Date(2024, 11, 5, 9, 30),
-      EndTime: new Date(2024, 11, 5, 11, 0)
+      EndTime: new Date(2024, 11, 5, 9, 0)
   }, {
       Id: 2,
       Subject: 'Thule Air Crash Report',
       StartTime: new Date(2024, 11, 8, 12, 0),
-      EndTime: new Date(2024, 11, 8, 14, 0)
+      EndTime: new Date(2024, 11, 8, 12, 30)
   }, {
       Id: 3,
       Subject: 'Blue Moon Eclipse',
       StartTime: new Date(2024, 11, 23, 9, 30),
-      EndTime: new Date(2024, 11, 23, 11, 0)
+      EndTime: new Date(2024, 11, 23, 10, 0)
   }, {
       Id: 4,
       Subject: 'Meteor Showers in 2024',
-      StartTime: new Date(2024, 11, 14, 13, 0),
-      EndTime: new Date(2024, 11, 14, 14, 30),
+      StartTime: new Date(2024, 11, 12, 7, 0),
+      EndTime: new Date(2024, 11, 12, 7, 30),
       IsBlock: true
-  }];
+  }, {
+      Id: 5,
+      Subject: 'Meteor Showers in 2024',
+      StartTime: new Date(2024, 11, 16, 8, 0),
+      EndTime: new Date(2024, 11, 16, 8, 30),
+    }, {
+      Id: 6,
+      Subject: 'Meteor Showers in 2024',
+      StartTime: new Date(2024, 11, 21, 7, 0),
+      EndTime: new Date(2024, 11, 21, 7, 30),
+    }, {
+      Id: 7,
+      Subject: 'Meteor Showers in 2024',
+      StartTime: new Date(2024, 11, 19, 13, 0),
+      EndTime: new Date(2024, 11, 19, 13, 30),
+    }];
 
-
-  const eventSettings: EventSettingsModel = { dataSource: eventData };
+  const eventSettings: EventSettingsModel = { 
+    dataSource: eventData,
+  };
 
   return (
     <div className="App">
       <ScheduleComponent
-      currentView="Month"
+        currentView="Month"
         height="650px"
-        selectedDate={new Date(2024, 11, 15)} 
-        eventSettings={eventSettings} 
+        selectedDate={new Date(2024, 11, 16)} 
+        eventSettings={eventSettings}
+        dragStart={onDragStart}
+        resizeStart={onResizeStart}
       >
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        <ViewsDirective>
+          <ViewDirective option="Day" />
+          <ViewDirective option="Week" eventTemplate={eventTemplate}  />
+          <ViewDirective option="Month" eventTemplate={eventTemplateMonth} />
+          <ViewDirective option="Agenda" />
+        </ViewsDirective>
+        <Inject services={[Day, Week, Month, Agenda, DragAndDrop, Resize]} />
       </ScheduleComponent>
     </div>
   );
